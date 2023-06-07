cb36059ba93b0b78c600263ff55a4c591fc70087
Hongwei Gao <hongwei.gao@bytedance.com>
2023-06-07 15:50:49 +0800

    Sync with master

--- init.lua
+++ init.lua
@@ -181,6 +181,9 @@ require('lazy').setup({
   {
     'nvim-telescope/telescope-fzf-native.nvim',
     build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+    cond = function()
+      return vim.fn.executable 'cmake' == 1
+    end,
   },
 
   {
